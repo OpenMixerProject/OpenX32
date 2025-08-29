a42ca728282988ad7c684e02f1da7b6adeed2fbe
Christian Nöding <christian@noeding-online.de>
2025-08-29 22:51:24 +0200

    Fixed compiler in dspconfig

--- software/dspconfig/compile.sh
+++ software/dspconfig/compile.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#/usr/bin/arm-linux-gnueabi-gcc dspconfig.c -static -o ../bin/dspconfig
-gcc dspconfig.c -o ../bin/dspconfig
+/usr/bin/arm-linux-gnueabi-gcc dspconfig.c -static -o ../bin/dspconfig
