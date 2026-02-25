c70c0695fda24f02d8cbc128ca4206a18f286565
Alexander Schulz <code@schulzalex.de>
2026-02-25 21:56:02 +0100

    optimized parameter "bs" for better compile speed

--- compile_all.sh
+++ compile_all.sh
@@ -66,19 +66,19 @@ cd ..
 rm /tmp/openx32.bin
 # Miniloader at offset 0x000000: will be started by i.MX Serial Download Program
 echo "     0% Copying Miniloader..."
-dd if=miniloader/miniloader.bin of=/tmp/openx32.bin bs=1 conv=notrunc > /dev/null 2>&1
+dd if=miniloader/miniloader.bin of=/tmp/openx32.bin conv=notrunc > /dev/null 2>&1
 # U-Boot at offset 0x0000C0: will be started by Miniloader
 echo "    20% Copying U-Boot..."
-dd if=u-boot/u-boot.bin of=/tmp/openx32.bin bs=1 seek=$((0xC0)) conv=notrunc > /dev/null 2>&1
+dd if=u-boot/u-boot.bin of=/tmp/openx32.bin bs=8 seek=$((0x18)) conv=notrunc > /dev/null 2>&1
 # Linux-Kernel at offset 0x060000 (384 kiB for Miniloader + U-Boot): will be started by U-Boot
 echo "    40% Copying Linux-Kernel...."
-dd if=/tmp/uImage of=/tmp/openx32.bin bs=1 seek=$((0x60000)) conv=notrunc > /dev/null 2>&1
+dd if=/tmp/uImage of=/tmp/openx32.bin bs=512 seek=$((0x300)) conv=notrunc > /dev/null 2>&1
 # DeviceTreeBlob at offset 0x800000 (~8 MiB for Kernel)
 echo "    60% Copying DeviceTreeBlob..."
-dd if=linux/arch/arm/boot/dts/nxp/imx/imx25-pdk.dtb of=/tmp/openx32.bin bs=1 seek=$((0x800000)) conv=notrunc > /dev/null 2>&1
+dd if=linux/arch/arm/boot/dts/nxp/imx/imx25-pdk.dtb of=/tmp/openx32.bin bs=512 seek=$((0x4000)) conv=notrunc > /dev/null 2>&1
 # InitramFS at offset 0x810000 (~64kiB for DeviceTreeBlob)
 echo "    80% Copying initramfs..."
-dd if=/tmp/uramdisk.bin of=/tmp/openx32.bin bs=1 seek=$((0x810000)) conv=notrunc > /dev/null 2>&1
+dd if=/tmp/uramdisk.bin of=/tmp/openx32.bin bs=512 seek=$((0x4080)) conv=notrunc > /dev/null 2>&1
 echo "   100% Add some zeros at the end of the binary-file..."
 dd if=/dev/zero of=/tmp/openx32.bin bs=1 count=100 oflag=append conv=notrunc > /dev/null 2>&1
 
