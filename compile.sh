97c97634a2716f124c365ed8761e5e4f578de960
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2026-04-16 07:53:03 +0600

    add missing libgcc for musl
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- compile.sh
+++ compile.sh
@@ -27,7 +27,7 @@ echo "                             .#@@%%*-.    .:=+**##***+.                   
 echo "                                  .-+%%%%%%#***=-.                                               "
 echo ""
 echo "Compiling OpenX32 Operating System for the Behringer X32 Audio-Mixing Console"
-
+export PATH=/opt/cross/bin:$PATH
 cleanup() {
     tput csr 0 $(($(tput lines) - 1)) # reset scroll-region
     tput rc                          # restore cursor
@@ -244,6 +244,7 @@ cp $(arm-linux-gnueabi-gcc -print-file-name=libcrypt.so.1) initramfs_root/lib/li
 # for musl
 #cp $(arm-linux-gnueabi-gcc -print-file-name=libc.so) initramfs_root/lib/libc.so
 #cp $(arm-linux-gnueabi-gcc -print-file-name=libstdc++.so.6) initramfs_root/lib/libstdc++.so.6
+#cp $(arm-linux-gnueabi-gcc -print-file-name=libgcc_s.so.1) initramfs_root/lib/libgcc_s.so.1
 #cd initramfs_root/lib/ && ln -sf libc.so ld-musl-arm.so.1 && cd ../../
 
 arm-linux-gnueabi-strip initramfs_root/lib/*
