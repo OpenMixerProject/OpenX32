1dee1d9e30478e40e62c5df5597661dd17cface9
Christian Nöding <christian@noeding-online.de>
2026-06-20 00:46:40 +0200

    Enabled all 16 Mixbusses

--- dsp/dsp1/src/defines.h
+++ dsp/dsp1/src/defines.h
@@ -12,7 +12,7 @@
 #define DEBUG_DISABLE_INTPUTDELAY	0
 #define DEBUG_DISABLE_OUTPUTDELAY	0
 
-#define ACTIVE_MIX_BUSSES		8	// 16 MixBusses are possible now
+#define ACTIVE_MIX_BUSSES		16	// 16 MixBusses are possible now
 
 #define DSP_BUF_IDX_OFF			0	// no audio
 #define DSP_BUF_IDX_DSPCHANNEL	1	// DSP-Channel 1-32
